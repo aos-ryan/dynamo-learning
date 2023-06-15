54d2281e29238ed1ee87ceb0a3a5d62a1e602606
ryan <ryan@arrayofstars.com>
2023-06-15 18:27:35 -0400

    update credentials object

--- netlify/functions/read-write-db.js
+++ netlify/functions/read-write-db.js
@@ -7,8 +7,8 @@ exports.handler = async function (event, context) {
   const client = new DynamoDBClient({
     region: 'us-east-2',
     credentials: {
-      'accessKeyId': process.env.MY_AWS_ACCESS_KEY_ID,
-      'secretAccessKey': process.env.MY_AWS_SECRET_ACCESS_KEY,
+      AccessKeyId: process.env.MY_AWS_ACCESS_KEY_ID,
+      SecretAccessKey: process.env.MY_AWS_SECRET_ACCESS_KEY,
     },
   })
   const docClient = DynamoDBDocumentClient.from(client)
