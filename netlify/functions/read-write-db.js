d3387da660d92fd40338c72b6ff2fac127e3ace6
ryan <ryan@arrayofstars.com>
2023-06-15 19:08:56 -0400

    fix netlify function to use require statement

--- netlify/functions/read-write-db.js
+++ netlify/functions/read-write-db.js
@@ -1,23 +1,21 @@
 /* eslint quote-props: 0 */
 
-import { DynamoDBClient } from '@aws-sdk/client-dynamodb'
-import { DynamoDBDocumentClient, ScanCommand } from '@aws-sdk/lib-dynamodb'
+const { DynamoDBClient, ScanCommand } = require('@aws-sdk/client-dynamodb')
 
 exports.handler = async function (event, context) {
   const client = new DynamoDBClient({
     region: 'us-east-2',
     credentials: {
-      AccessKeyId: process.env.MY_AWS_ACCESS_KEY_ID,
-      SecretAccessKey: process.env.MY_AWS_SECRET_ACCESS_KEY,
+      accessKeyId: process.env.MY_AWS_ACCESS_KEY_ID,
+      secretAccessKey: process.env.MY_AWS_SECRET_ACCESS_KEY,
     },
   })
-  const docClient = DynamoDBDocumentClient.from(client)
 
   const command = new ScanCommand({
     TableName: 'Users',
   })
 
-  const response = await docClient.send(command)
+  const response = await client.send(command)
 
   return {
     statusCode: 200,
